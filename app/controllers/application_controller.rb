63dc88a42350fd3fd6c21c9614b79ba510dc1b07
daiki.takeishi <tw.dia3@gmail.com>
2021-03-13 06:20:32 +0000

    follow

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,8 @@ class ApplicationController < ActionController::Base
   
   def counts(user)
     @count_infos = user.infos.count
+    @count_followings = user.followings.count
+    @count_followers = user.followers.count
   end
   
 end
